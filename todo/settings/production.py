f57f95d8a2f8a43a41c7ef6b310946bea70a0e47
nikhil0162 <nikhil0162@gmail.com>
2021-02-26 10:45:07 +0530

    adding allowed hosts to production file

--- todo/settings/production.py
+++ todo/settings/production.py
@@ -5,7 +5,8 @@ if settings.PRODUCTION_READY:
     from .base import *
     SECRET_KEY = '8f3gg$1*l(2%!p%rot2q9a4xb1!u8o78)$o9irc=$)63f42-rq'
     DEBUG = True
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['ec2-13-234-30-160.ap-south-1.compute.amazonaws.com',
+                     '.ec2-13-234-30-160.ap-south-1.compute.amazonaws.com']
 
     # Database
     # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
